7872e3ced74768280ed690b52a815397798ab346
Leeuenho <80818827+Leeuenho@users.noreply.github.com>
2021-10-24 04:23:00 +0900

    randomforest 정확도 측정

--- wfdb_filteredData_random_forest.py
+++ wfdb_filteredData_random_forest.py
@@ -1,13 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 20 07:27:43 2021
+
+@author: eunho
+"""
+
+
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
-import os
+from sklearn.model_selection import validation_curve
+from sklearn.model_selection import GridSearchCV
 import numpy as np
+import matplotlib.pyplot as plt
+import os
+import scipy.stats as sc
+
+
+import warnings
+warnings.filterwarnings('ignore')
 
 path = 'D:/한림대학교/연구실/'
+
 test_path = path+'test_data//'
 
 test_name = []
@@ -18,52 +34,45 @@ for file in test_file_list:
     if file.count(".") ==1:
         name = file.split('.')[0]
         test_name.append(name[-3:])
-        if name.startswith('wfdb') : test_file_name.append(name) 
-       
-
+        test_file_name.append(name)
 
 train_data = pd.read_csv(path+'wfdb_filteredData.csv')
 train_data.rename(columns={'Unnamed: 0':'name'},inplace=True)
 X_train = train_data.drop(['y_train','name'],axis=1)
 y_train = train_data['y_train']
 
-import scipy.stats as ss
 X_t = X_train.transpose()
-z_X_train_t = ss.zscore(X_t)
-z_X_train = z_X_train_t.transpose()
+X_t = sc.zscore(X_t)
+X_t = X_t.transpose()
 
 model = RandomForestClassifier(oob_score=True)
-model.fit(X_train,y_train)
+model.fit(X_t,y_train)
 print("data_oob_score : ",model.oob_score_)
 
-z_model = RandomForestClassifier(oob_score=True)
-z_model.fit(z_X_train,y_train)
-print("std_data_oob_score : ",z_model.oob_score_)
-
-idx_index = ['test_accuracy','Sensitivity','Precision','z_accuracy','z_Sensitivity','z_Precision']
-result_data = pd.DataFrame(index = idx_index)
-
-for i in range(0,(len(test_name)-1)):
-    test_data = pd.read_csv(test_path + test_file_name[i] + '.csv')
+Xdata_out=[]
+for i in range(0, (len(test_name)-1)):
+    test_data = pd.read_csv(test_path+test_file_name[i]+'.csv')
     test_data.rename(columns={'Unnamed: 0':'name'},inplace=True)
     X_test = test_data.drop(['y_train','name'],axis=1)
-    y_true = test_data['y_train']
     
+    X_test = X_test.transpose()
+    X_test = sc.zscore(X_test)
+    X_test = X_test.transpose()
     pred = model.predict(X_test)
-    accuracy = accuracy_score(y_true, pred)
-    Sensitivity = recall_score(y_true,pred)
-    Precision = precision_score(y_true,pred)
-    
-    X_test_t = X_test.transpose()
-    z_X_test_t = (X_test_t - np.mean(X_test_t,axis=0)/np.std(X_test_t,axis=0))
-    z_X_test = z_X_test_t.transpose()
-    
-    z_pred = model.predict(z_X_test)
-    z_accuracy = accuracy_score(y_true,z_pred)
-    z_Sensitivity = recall_score(y_true,z_pred)
-    z_Precision = precision_score(y_true,z_pred)
-    
-    result_data.insert(result_data.shape[1], test_name[i], [accuracy,Sensitivity,Precision,z_accuracy,z_Sensitivity,z_Precision])
-result = result_data.transpose()
+    ap=0;non=0
+    for k in range(len(pred)):
+        if pred[k]==1: ap +=1;
+        if pred[k]==0: non +=1;
+    Xdata_out.append(np.round(ap/(non+1)*100,1))
 
+Xdata_y=np.array([63,37.7,0.13,0,34,0,21,48,18.5,10,5,33,18.7,79.5,
+                  15.9,24,0,0,56.2,43,19,0,14.3,0,48,15.1,75,75,0,41,93.5,71.8,0.13,0.38,0])
+Xdata_yt=np.array([1,1,0,0,1,0,1,1,1,0,0,1,1,1,1,1,0,0,1,1,1,0,0,0,1,1,1,1,0,1,1,1,0,0,0])
 
+xt=np.arange(1,36,1)    
+plt.rcParams['figure.figsize']=(10,5)
+plt.plot(xt,Xdata_out,'o',color='b')
+plt.plot(xt,Xdata_yt*50,'o',color='r')
+#plt.plot(xt,Xdata_y,'x')
+plt.axhline(y=30)
+plt.xticks(xt)
